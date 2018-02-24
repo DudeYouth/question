31e881f3ba2c7e4bf0f29c3d48979fd41a89768c
git <1157536217@qq.com>
2018-02-24 18:05:51 +0800

    test

---
+++ export.php
@@ -0,0 +1,197 @@
+<?php
+    // require_once 'vendor/autoload.php';
+    // use PHPExcel\PHPExcel;
+    // use PHPExcel\Writer\Excel2007;
+    // use PHPExcel\Writer\Excel5;
+    require 'PHPExcel/Classes/PHPExcel.php';
+    require 'PHPExcel/Classes/PHPExcel/Writer/Excel2007.php';
+    require 'PHPExcel/Classes/PHPExcel/Writer/Excel5.php';
+    set_time_limit(0);
+    function getImages(){
+        $data = file_get_contents('result.json');
+        $arr = json_decode($data,true);
+        $reg = "/<img[\s\S]+src=\"(.*?)\".*?>/";
+        $type_reg = "/\.[a-zA-Z0-9]+$/";
+        foreach($arr as &$v){
+            if( preg_match($reg,$v['question'],$res1) ){
+                $image_src = $res1[1];
+                if( $image_src ){
+                    if( preg_match($type_reg,$image_src,$res3) ){
+                        $images_name = 'question_'.time().'_'.rand(1,100000);
+                        $type = $res3[0];
+                        try{
+                            $image = file_get_contents($image_src);
+                            $v['images'] = $images_name.$type;
+                            file_put_contents('./images/'.$images_name.$type,$image);
+                        }catch(Exception $e){
+
+                        }
+
+                    }
+
+                }
+            }
+            $v['images'] = [];
+            foreach($v['option'] as $key=>&$value ){
+                if( preg_match($reg,$value,$res2) ){
+                    $image_src = $res2[1];
+                    if( $image_src ){
+                        if( preg_match($type_reg,$image_src,$res4) ){
+                            $images_name = 'option_'.time().'_'.rand(1,100000);
+                            $type = $res4[0];
+                            try{
+                                $image = file_get_contents($image_src);
+                                $v['images'][$key] = $images_name.$type;
+                                file_put_contents('./images/'.$images_name.$type,$image);
+                            }catch(Exception $e){
+
+                            }
+
+                        }
+                    }
+                }              
+            }
+
+        }
+        return $arr;
+    }
+    function exportExcel2($expTitle,$expCellName,$expTableData,$tablename=''){
+        $xlsTitle = iconv('utf-8', 'gb2312', $expTitle);//文件名称 
+        $fileName = $xlsTitle;//$_SESSION['account'].date('_YmdHis');//or $xlsTitle 文件名称可根据自己情况设定
+        $cellNum = count($expCellName);//列
+        $dataNum = count($expTableData);//行
+		$objPHPExcel = new \PHPExcel();
+        //p([123,$objPHPExcel]);die;
+        $cellName = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','AA','AB','AC','AD','AE','AF','AG','AH','AI','AJ','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AW','AX','AY','AZ');
+        $objPHPExcel->getActiveSheet(0)->mergeCells('A1:'.$cellName[$cellNum-1].'1');//合并单元格
+        $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A1',$tablename );
+        $objPHPExcel->getActiveSheet()->getStyle('A1')->getAlignment()->setHorizontal(\PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+        for($i=0;$i<$cellNum;$i++){
+            if(strpos($expCellName[$i][1],'=') === 0){
+                $expCellName[$i][1] = "'".$expCellName[$i][1];
+            }
+            $objPHPExcel->setActiveSheetIndex(0)->setCellValue($cellName[$i].'2', $expCellName[$i][1]); 
+        }
+        for($i=0;$i<$dataNum;$i++){
+          for($j=0;$j<$cellNum;$j++){
+              if( isset($expTableData[$i][$expCellName[$j][0]]) ){
+                    if( strpos($expTableData[$i][$expCellName[$j][0]],'=') === 0 ){
+                        $expTableData[$i][$expCellName[$j][0]] = "'".$expTableData[$i][$expCellName[$j][0]];
+                    }
+                    $objPHPExcel->getActiveSheet(0)->setCellValue($cellName[$j].($i+3), $expTableData[$i][$expCellName[$j][0]]);
+              }
+
+                
+                //$objPHPExcel->getActiveSheet()->getStyle($cellName[$j].($i+3))->getAlignment()->setWrapText(true);
+          }             
+        }  
+        for($j=0;$j<$cellNum;$j++){
+            $objPHPExcel->getActiveSheet()->getColumnDimension($cellName[$j])->setAutoSize(true);
+            //$objPHPExcel->getActiveSheet()->getStyle($cellName[$j].($i+3))->getAlignment()->setWrapText(true);
+      }
+        header('pragma:public');
+        header('Content-type:application/vnd.ms-excel;charset=utf-8;name="'.$xlsTitle.'.xls"');
+        header("Content-Disposition:attachment;filename=$fileName.xls");//attachment新窗口打印inline本窗口打印      
+        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');  
+        $objWriter->save('php://output'); 
+        exit;   
+    }
+    //平台导出报量结果
+    function exp_company(){
+        // $data = file_get_contents('result.json');
+        // $xlsData = json_decode($data,true);
+        $xlsData =  getImages();
+        die;
+        $xlsName  = "题库表";
+        $tablename = "题库表";
+        $new_data = [];
+        $questions = [];
+        $xlsCell  = [
+            ['question','题目'],
+            ['answer1','选项1'],
+            ['answer2','选项2'],
+            ['answer3','选项3'],
+            ['answer4','选项4'],
+            ['type','题目类型'],
+            ['level','题目等级'],
+            ['answer','答案'],
+            ['analysis','解析'],
+            ['image1','图1'],
+            ['image2','图2'],
+            ['image3','图3'],
+            ['image4','图4'],
+        ];
+        foreach( $xlsData as $v){
+            if( !in_array($v['question'],$questions) ){
+                $questions[] = $v['question'];
+                $json = [];
+                $json['question'] = $v['question'];
+                $json['answer1'] = $v['option'][0];
+                $json['answer2'] = $v['option'][1];
+                if( isset($v['option'][2]) ){ 
+                    $json['answer3'] = $v['option'][2];
+                }
+                if( isset($v['option'][3]) ){ 
+                    $json['answer4'] = $v['option'][3];
+                }
+                if( isset($v['images'][0]) ){ 
+                    $json['image1'] = $v['images'][0];
+                }
+                if( isset($v['images'][1]) ){ 
+                    $json['image2'] = $v['images'][1];
+                }
+                if( isset($v['images'][2]) ){ 
+                    $json['image3'] = $v['images'][2];
+                }
+                if( isset($v['images'][3]) ){ 
+                    $json['image4'] = $v['images'][3];
+                }
+                $json['answer'] =  $v['answer'];
+                $json['analysis'] =  $v['analysis'];
+                switch( $v['name'] ){
+                    case '语文':
+                        $json['type'] = 1;
+                        break;
+                    case '数学':
+                        $json['type'] = 2;
+                        break;
+                    case '英语':
+                        $json['type'] = 3;
+                        break;  
+                    default:
+                        $json['type'] = 1;
+                        break;                     
+                }
+                $level = substr($v['level'],0,3);
+                switch( $level ){
+                    case '一':
+                        $json['level'] = 1;
+                        break;
+                    case '二':
+                        $json['level'] = 2;
+                        break;
+                    case '三':
+                        $json['level'] = 3;
+                        break;  
+                    case '四':
+                        $json['level'] = 4;
+                        break; 
+                    case '五':
+                        $json['level'] = 5;
+                        break; 
+                    case '六':
+                        $json['level'] = 6;
+                        break; 
+                    default:
+                        $json['level'] = 1;
+                        break;                     
+                }
+                $new_data[] = $json;
+            }
+
+            
+        }
+        //$xlsData  = $xlsModel->Field('id,act_id,drug_id,status')->select();
+        exportExcel2($xlsName,$xlsCell,$new_data,$tablename);    
+    }
+    exp_company();
--- index.php
+++ index.php
@@ -1,17 +1,58 @@
 <?php
 
+function request($url,$type='get',$param=[]){
+	$ch = curl_init(); 
+	// 返回结果存放在变量中，不输出 
+	curl_setopt($ch, CURLOPT_URL, $url);
+	if( $type=='post' ){
+		curl_setopt($ch, CURLOPT_POST,true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($param));
+		$headers_login = [
+			"User-Agent:Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.18 Safari/537.36",
+			"Host:www.tiku.cn",
+			"Origin:http://www.tiku.cn",
+			"X-Requested-With:XMLHttpRequest",
+			"Accept:application/json, text/javascript, */*; q=0.01",
+			"Cookie:PHPSESSID=1p6fpkemmn3isc5kltr3uk2suu; Hm_lvt_02f32149c7ea90d0cd47ed89025e457c=1518074988,1518315926,1519368479,1519374574; token=ea8d1a2b2e637c665e6640b8bc4619cc; userId=10678; Hm_lpvt_02f32149c7ea90d0cd47ed89025e457c=".time(),
+		];
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers_login); 
+	}
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	//curl_setopt($ch, CURLOPT_COOKIEFILE, 'PHPSESSID=sjfask4v7cmtlkqe65vqvkhnot; Hm_lvt_02f32149c7ea90d0cd47ed89025e457c=1517987272,1518074988,1518315926,1519368479; token=ea8d1a2b2e637c665e6640b8bc4619cc; userId=10678; Hm_lpvt_02f32149c7ea90d0cd47ed89025e457c=1519372429');
+	//curl_setopt($ch, CURLOPT_HEADER, 1); 
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 120); 
+	//curl_setopt($ch,CURLOPT_COOKIEJAR,'./cookie.txt'); 
+	//curl_setopt($ch,CURLOPT_COOKIEFILE,'./cookie.txt'); 
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$result= curl_exec($ch);
+	curl_close($ch);
+    file_put_contents('request.log',$url."\r\n",FILE_APPEND);
+    return $result;
+}
+
 class QuestionsCrawler{
     public function __construct(){
         $this ->enter = 'http://www.tiku.cn';
+		//$reg = "/<span class=\"view-err\" onclick=\"show_anaylis\(\\$\(this\),(\d+)\)\"/";  // 获取答案id
+		//$content = file_get_contents('test1.html');
+		//preg_match($reg,$content,$res);
+		//echo json_encode([$res,$content]);die;
+		
         // $this ->getContent();
         $this ->res = [];
+        $this ->sub_name = '';  // 科目名
+        $this ->sub_type = '';  // 题目类型
+        $this ->sub_level = ''; // 题目等级
+		//request('http://www.tiku.cn/api/question/anylysis/','post',['id'=>3025439]);
         //$this ->get_question_type();
-        $text = file_get_contents('test.html');
-        echo $this ->get_question_page($text);
-        //$this ->get_level_data($this ->enter);
+        //$this ->get_questions($text);
+		$this ->answer = ['A'=>1,'B'=>2,'C'=>3,'D'=>4,'a'=>1,'b'=>2,'c'=>3,'d'=>4];
+        $this ->get_level_type_data('');
+        // $text = request('test.html');
+        // $this ->get_question_radio_id($text);
     }
     public function get_level_data($url){
-        $text = file_get_contents($url);
+        $text = request($url);
         $reg1 = "/<div class=\"subject\">([\s\S]*)<\/div>[\n\s\r]*<div class=\"data\">/"; // 获取题库类型内容主题
         $reg2 = "/<table class=\"inner_table\">([\s\S]*?)<\/table>/"; // 获取每个类型的内容
         if( preg_match($reg1,$text,$res1) ){
@@ -35,28 +76,42 @@ class QuestionsCrawler{
         $arr = [];
         $reg = "/<span>([\x80-\xff]{1,}) \| <\/span>[\n\s\r]*<span><a href=\"(\/index\/index\/questions\?cid=(\d+)&cno=1)\"[\n\s\r]*?target=\"_blank\">试题<\/a>/";
         //file_put_contents('test.html','<html><head><meta charset="utf-8 />"</head><body>'.$text.'</body><html>');
-        if( preg_match_all($reg,$text,$res) ){
+        // if( preg_match_all($reg,$text,$res) ){
             //echo json_encode($res);
             //file_put_contents('test.html','<html><head><meta charset="utf-8 />"</head><body>'.$res[1][0].'</body><html>');
-            foreach( $res[1] as $k=>$v ){
-                $arr[$res[2][$k]] = $this ->get_question_type($this ->enter.$v);
+        //     foreach( $res[2] as $k=>$v ){
+        //         $arr[$res[3][$k]] = [
+        //             'name'=>$v,
+        //             'content'=>$this ->get_question_type($this ->enter.$v)
+        //         ];
+        //     }
+        // }
+        $types = [['/index/index/questions?cid=14&cno=1','数学'],['/index/index/questions?cid=21&cno=1','语文'],['/index/index/questions?cid=23&cno=1','英语']];
+            foreach( $types as $k=>$v ){
+                $this ->sub_name = $v[1];
+                $this ->get_question_type($this ->enter.$v[0]);
+                // $arr[$k] = [
+                //     'name'=>$v[1],
+                //     'content'=>$this ->get_question_type($this ->enter.$v[0])
+                // ];
             }
-        }
     }
     // 获取课目下的题目类型
     public function get_question_type($url=''){
         $arr = [];
-        $text = file_get_contents($url);
+        $text = request($url);
         $reg1 = "/<ul class=\"ul-ques\">([\s\S]*?)<\/ul>/";
         $reg2 = "/<b onclick=\"tourl\((\d+),'unitid'\)\"[\s\S]+?>([\s\S]+?)<\/b>/";
         if( preg_match($reg1,$text,$res1) ){
             if( !empty($res1[1]) ){
                 if( preg_match_all($reg2,$res1[1],$res2) ){
                     foreach( $res2[1] as $k=>$v ){
-                        $arr[] = [
-                            'type'=>$res2[2][$k],
-                            'content'=>$this ->get_all_questions($url.'&unitid='.$v)
-                        ];
+                        $this ->sub_type = $res2[2][$k];
+                        $this ->get_all_questions($url.'&unitid='.$v);
+                        // $arr[] = [
+                        //     'type'=>$res2[2][$k],
+                        //     'content'=>$this ->get_all_questions($url.'&unitid='.$v)
+                        // ];
                     }
                 }
             }
@@ -66,8 +121,8 @@ class QuestionsCrawler{
     // 获取课目下的年级类型
     public function get_question_nia_id($text){
         $arr = [];
-        $reg1 = "/<div class=\"tp-sub-list\">([\s\S]*?)<\/div>[\s\n\r]*?<\/div>/";
-        $reg2 = "/<div class=\"tp-select type-c [\s[0-9a-zA-Z]*?\" id=\"[0-9a-zA-Z]+?\" onclick=\"tourl\((\d+),'bid'\)\">([\u4e00-\u9fa5]+)<\/div>/";
+        $reg1 = "/<div class=\"nav-select-list\" id=\"book\">([\s\S]*?)<\/div>[\s\n\r]*?<\/div>[\s\n\r]*?<\/div>/";
+        $reg2 = "/<div class=\"tp-select type-c [\s\S]+?onclick=\"tourl\((\d+),'bid'\)\">([\x80-\xff]{1,})/";
         if( preg_match($reg1,$text,$res1) ){
             if( preg_match_all($reg2,$res1[1],$res2) ){
                 foreach( $res2[1] as $k=>$v ){
@@ -82,25 +137,27 @@ class QuestionsCrawler{
     }
     // 获取单选的id
     public function get_question_radio_id($text){
-        $reg1 = "/<div class=\"tp-sub-list\">([\s\S]*?)<\/div>[\s\n\r]*?<\/div>/";
-        $reg2 = "/<div class=\"tp-select type-c [\s[0-9a-zA-Z]*?\" id=\"[0-9a-zA-Z]+?\" onclick=\"tourl\((\d+),'bid'\)\">单选题<\/div>/";
-        if( preg_match($reg1,$text,$res1) ){
-            if( preg_match($reg2,$res1[1],$res2) ){
-                return $res2[1];
-            }           
-        }
+        $reg2 = "/<div class=\"tp-select type-d [\s[0-9a-zA-Z]*?\" id=\"[0-9a-zA-Z]+?\" onclick=\"tourl\((\d+),'typeid'\)\">单选题<\/div>/";
+        if( preg_match($reg2,$text,$res2) ){
+            return $res2[1];
+        }           
         return false;
     }
     public function get_all_questions($url){
         $arr = [];
-        $text = file_get_contents($url);
+        $text = request($url);
         $nia_id = $this ->get_question_nia_id($text);
         $radio_id = $this ->get_question_radio_id($text);
-        foreach( $nia_id as $v ){
-            $arr[] = [
-                        'name'=>$v['name'],
-                        'content'=>$this ->get_all_page_question( $url.'&bid='.$v['id'] ),
-            ];
+        echo $radio_id;
+        if( $radio_id ){
+            foreach( $nia_id as $v ){
+                $this ->sub_level = $v['name'];
+                $this ->get_all_page_question( $url.'&bid='.$v['id'].'&typeid='.$radio_id );
+                // $arr[] = [
+                //             'name'=>$v['name'],
+                //             'content'=>$this ->get_all_page_question( $url.'&bid='.$v['id'].'&typeid='.$radio_id ),
+                // ];
+            }
         }
         return $arr;
 
@@ -108,8 +165,9 @@ class QuestionsCrawler{
     // 获取所有页面的题目数据
     public function get_all_page_question($url=''){
         $arr = [];
-        $text = file_get_contents('test.html');
+        $text = request($url);
         $page = $this ->get_question_page($text);
+        echo $page;
         for( $i=1;$i<=$page;$i++ ){
             $data = $this ->get_questions($url.'&page='.$i);
             $arr = array_merge($arr,$data);
@@ -118,36 +176,60 @@ class QuestionsCrawler{
     }
     // 获取单页面的所有题目数据
     public function get_questions($url){
-        $text = file_get_contents($url);
-        $reg1 = "/<div class=\"ques\">>([\s\S]+?)<!--<div class=\"clearfix\"><\/div>-->[\r\n\s]*<\/div>/";  // 获取某道题
-        $reg2 = "/<div class=\"q-title\">([\s\S]+?)<\/div>/";  // 获取问题
+        sleep(1);
+        $text = request($url);
+        $reg1 = "/<div class=\"ques\">([\s\S]+?)<!--<div class=\"clearfix\"><\/div>-->[\r\n\s]*<\/div>/";  // 获取某道题
+        $reg2 = "/<div class=\"q-title\">([\s\S]+?)<br>/";  // 获取问题
         $reg3 = "/<div class=\"answer\">[a-zA-Z]{1}、([\s\S]+?)<\/div>/";  // 获取选项
+		$reg4 = "/<span class=\"view-err\" onclick=\"show_anaylis\(\\$\(this\),(\d+)\)\"/";  // 获取答案id
         $arr = [];
+		$answer = '';
+		$analysis = '';
         if( preg_match_all($reg1,$text,$res1) ){
             foreach( $res1[1] as $v1 ){
                 if( preg_match($reg2,$v1,$res2) ){
                     $question = $res2[1];
-                    $answer = [];
-                    if( preg_match_all($reg3,$title,$res3) ){
+                    $option = [];
+                    if( preg_match_all($reg3,$v1,$res3) ){
                         foreach( $res3[1] as $v2 ){
-                            $answer[] = $v2;
+                            $option[] = trim($v2);
                         }
                     }
+					$answer = '';
+					$analysis = '';
+					//preg_match($reg4,$v1,$res4);
+					if( preg_match($reg4,$v1,$res4) ){
+						echo $res4[1];
+						$result = request('http://www.tiku.cn/api/question/anylysis/','post',['id'=>$res4[1]]);
+						$result = json_decode($result,true);
+						if( $result['msg']=='success' ){
+							$answer = $this ->answer[$result['data']['answer']];
+							$analysis = $result['data']['anylysisHtml'];			
+						}
+					}
                 }
-                $arr[] = ['question'=>$question,'answer'=>$answer];
-            }
+                if( !empty($option) ){
+                    $res = ['question'=>trim($question),'option'=>$option,'answer'=>$answer,'analysis'=>$analysis,'type'=>$this ->sub_type,'level'=>$this ->sub_level,'name'=>$this ->sub_name];
+                    $arr[] = $res;
+                    file_put_contents('result.json',json_encode($res,JSON_UNESCAPED_UNICODE).',',FILE_APPEND);
+                }
 
+            }
+            
         }
         return $arr;
     }
     // 获取某个类型单选题的分页数量
     public function get_question_page($text){
         $reg1 = "/<ul class=\"pagination\">([\s\S]+?)<\/ul>/";
-        $reg2 = "/<a href=[\s\S]+?>(\d+?)<\/a>[\r\n\s]*<a href=[\s\S]+?>»<\/a>/";
+        $reg2 = "/<a href=[\s\S]+?>(\d+?)<\/a><\/li>/";
         if( preg_match($reg1,$text,$res1) ){
-            echo json_encode($res1);
-            if( preg_match($reg2,$res1[1],$res2) ){
-                return $res2[1];
+            if( preg_match_all($reg2,$res1[1],$res2) ){
+                $len = count( $res2[1] );
+                if( $len ){
+                    return $res2[1][$len-1];
+                }
+                
             }
         }
         return false;
